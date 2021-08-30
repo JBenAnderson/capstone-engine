032f976001e9492be0422e2b2298209c03b911bb
J Ben Anderson <revovisions@gmail.com>
2021-08-30 19:56:52 -0400

    pre backend next.js framework with couple of restaurant files

---
+++ restaurant-app/pages/restaurants/[restaurant].js
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/Home.module.css";
+
+export default function Restaurant() {
+  const router = useRouter();
+  return (
+    <>
+      <h1>Dynamic Restaurant Page {router.query.restaurant}</h1>
+    </>
+  );
+}
