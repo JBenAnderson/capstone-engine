3b357ec18aeb578eaa1a5ce9814c1a8253a8da68
J Ben Anderson <revovisions@gmail.com>
2021-08-31 15:22:14 -0400

    sample restaurant setup with checkout --working alpha

--- restaurant-app/pages/index.js
+++ restaurant-app/pages/index.js
@@ -1,11 +1,38 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import React, { useState } from "react";
+import Cart from "../components/cart";
+import {
+  ApolloProvider,
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import RestaurantList from "../components/restaurantList.js";
+import { InputGroup, InputGroupAddon, Input } from "reactstrap";
 
-export default function Home() {
+function Home() {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
+  console.log(`URL: ${API_URL}`);
+  const [query, setQuery] = useState("");
+  const link = new HttpLink({ uri: `${API_URL}/graphql` });
+  const cache = new InMemoryCache();
+  const client = new ApolloClient({ link, cache });
+
   return (
-    <>
-      <h1>Restaurant Page</h1>
-    </>
+    <ApolloProvider client={client}>
+      <div className="search">
+        <h2> Local Restaurants</h2>
+        <InputGroup>
+          <InputGroupAddon addonType="append"> Search </InputGroupAddon>
+          <Input
+            onChange={(e) => setQuery(e.target.value.toLocaleLowerCase())}
+            value={query}
+          />
+        </InputGroup>
+        <br></br>
+      </div>
+      <RestaurantList search={query} />
+      <Cart> </Cart>
+    </ApolloProvider>
   );
 }
+export default Home;
